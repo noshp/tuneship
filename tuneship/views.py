e7d3f789c2d1a3ffe4499c6fbdd6be93e211e354
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-11-25 16:54:06 -0500

    added views

--- tuneship/views.py
+++ tuneship/views.py
@@ -1,6 +1,34 @@
-from flask import render_template
-from . import app
+from flask import render_template, request
+from . import app, db
+from tuneship.models import Data
+from tuneship.forms import EnterDBInfo, RetrieveDBInfo
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    form1 = EnterDBInfo(request.form)
+    form2 = RetrieveDBInfo(request.form)
+
+    if request.method == 'POST' and form1.validate():
+        data_entered = Data(notes=form1.dbNotes.data)
+        try:
+            db.session.add(data_entered)
+            db.session.commit()
+            db.session.close()
+        except:
+            db.session.rollback()
+        return render_template('thanks.html', notes=form1.dbNotes.data)
+
+    if request.method == 'POST' and form2.validate():
+        try:
+            num_return = int(form2.numRetrieve.data)
+            query_db = Data.query.order_by(Data.id.desc().limit(num_return))
+            for q in query_db:
+                print(q.notes)
+            db.session.close()
+        except:
+            db.session.rollback()
+        return render_template('results.html', results=query_db, num_return=num_return)
+
+    return render_template('index.html', form1=form1, form2=form2)
+      
