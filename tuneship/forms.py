ea82e11826d2fa334614bfa5fa9e47a3606fc8df
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-11-25 16:53:48 -0500

    added forms

---
+++ tuneship/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import TextField, validators
+
+class EnterDBInfo(FlaskForm):
+    dbNotes = TextField(label='Items to add to DB', description='db_enter',validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])
+
+class RetrieveDBInfo(FlaskForm):
+    numRetrieve = TextField(label='Number of DB items to get', description="db_get", validators=[validators.required(), validators.Regexp('^\d{1}$', message=u'Enter a number between 1 and 10')])
