f918a38aa0d795255792c7ba21dd59d6913eaac8
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-11-26 20:39:26 -0500

    added new model for holding tunes data

--- tuneship/models.py
+++ tuneship/models.py
@@ -9,3 +9,10 @@ class Data(db.Model):
     
     def __repr__(self):
         return '<Data %r>' % self.notes
+
+class TunesData(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(128), index=True, unique=False)
+    thumb_url = db.Column(db.String(128), index=True, unique=False)
+    media_url = db.Column(db.String(256), index=True, unique=True)
+    iframe_string = db.Column(db.String(256), index=True, unique=True)
