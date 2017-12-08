fc9560f24d546b6afd21e72128b6d0bc75492bfe
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-12-07 21:09:05 -0500

    new view for entering to the database for the tunesdata model

--- tuneship/views.py
+++ tuneship/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request
 from . import application, db
 from tuneship.models import Data, TunesData
-from tuneship.forms import EnterDBInfo, RetrieveDBInfo
+from tuneship.forms import EnterDBInfo, RetrieveDBInfo,EnterTunesData
 
 @application.route('/enterdb', methods=['GET', 'POST'])
 def enterdb():
@@ -30,6 +30,23 @@ def enterdb():
         return render_template('results.html', results=query_db, num_return=num_return)
 
     return render_template('enterdb.html', form1=form1, form2=form2)
+
+@application.route('/entertunes', methods=['GET', 'POST'])
+def entertunes():
+    tunesForm = EnterTunesData(request.form)
+
+    if request.method == 'POST' and tunesForm.validate():
+        data_entered = TunesData(title=tunesForm.title.data,thumb_url=tunesForm.thumb_url.data, 
+                                 media_url=tunesForm.media_url.data, iframe_string=tunesForm.iframe_string.data)
+        try:
+            db.session.add(data_entered)
+            db.session.commit()
+            db.session.close()
+        except:
+            db.session.rollback()
+        return render_template('thanks.html', notes=tunesForm.title.data)
+    
+    return render_template('entertunesdb.html',tunesForm=tunesForm)
 
 @application.route('/')
 def index():
