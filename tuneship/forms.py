249cd847cd3ddac611c9d50c73a88efa74daba62
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-12-07 21:08:18 -0500

    new form for testing submission

--- tuneship/forms.py
+++ tuneship/forms.py
@@ -6,3 +6,9 @@ class EnterDBInfo(FlaskForm):
 
 class RetrieveDBInfo(FlaskForm):
     numRetrieve = TextField(label='Number of DB items to get', description="db_get", validators=[validators.required(), validators.Regexp('^\d{1}$', message=u'Enter a number between 1 and 10')])
+
+class EnterTunesData(FlaskForm):
+    title = TextField(label='Title of media link', description='db_enter', validators=[validators.required(),validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])
+    thumb_url = TextField(label='Link for thumbnail image', description='db_enter', validators=[validators.required(),validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])
+    media_url = TextField(label='Media link', description='db_enter', validators=[validators.required(),validators.Length(min=0, max=256, message=u'Enter 256 characters or less')])
+    iframe_string = TextField(label='iframe string', description='db_enter', validators=[validators.required(),validators.Length(min=0, max=256, message=u'Enter 128 characters or less')])
